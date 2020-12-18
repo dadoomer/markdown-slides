9b79ba7fdf0845a320a983dd349bf2d305e15fae
Leo <leohdz.c0@gmail.com>
2020-12-17 23:41:10 -0600

    Fixed potential bug

--- mdslides/__main__.py
+++ mdslides/__main__.py
@@ -23,7 +23,7 @@ def main():
 
     revealjs_origin = resource_path/"reveal.js"
     markdown_file = target_path/Path(args.FILE)
-    revealjs_dir = markdown_file.parent/markdown_file.with_suffix('')
+    revealjs_dir = target_path/markdown_file.with_suffix('')
     index_file_original = target_path/revealjs_dir/ "index_template.html"
     index_file_new = target_path/revealjs_dir/ "index.html"
 
