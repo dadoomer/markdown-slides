b3b97175823f691a5a091bee0c962c797f5bb8a1
Leo <leohdz.c0@gmail.com>
2021-01-10 15:11:19 -0600

    Updated package data in setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,15 @@ from pathlib import Path
 revealjsdata = glob.glob("mdslides/reveal.js/**", recursive=True)
 revealjsdata = [str(Path(p).relative_to("mdslides")) for p in revealjsdata]
 
+katexdata = glob.glob("mdslides/KaTeX/**", recursive=True)
+katexdata = [str(Path(p).relative_to("mdslides")) for p in katexdata]
+
+mdslidesdata = revealjsdata + katexdata
+
 setup(name='MarkdownSlides',
       version='1.1',
       description='Write modern slides with markdown.',
       packages=['mdslides'],
       entry_points={'console_scripts': ['mdslides=mdslides.__main__:main']},
-      package_data={'mdslides': revealjsdata},
+      package_data={'mdslides': mdslidesdata},
       )
