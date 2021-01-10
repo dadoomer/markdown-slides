1af5e4ad7426eefab420de62a3609dc301b4ab12
Leo <leohdz.c0@gmail.com>
2021-01-10 12:48:45 -0600

    Code style improvements

--- mdslides/__main__.py
+++ mdslides/__main__.py
@@ -1,16 +1,29 @@
 #!/bin/python3.8
 from pathlib import Path
 import argparse
-from zipfile import ZipFile
 import shutil
 import subprocess
 import re
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Convert a markdown file to a reveal.js presentation.')
+    parser = argparse.ArgumentParser(
+            description='Convert a markdown file to a reveal.js presentation.'
+        )
     parser.add_argument('FILE', type=str, help="Markdown file.")
-    parser.add_argument('--include', metavar="RESOURCE", help="Directory or file to include. This option can be used multiple times.", default=[], action="append")
-    parser.add_argument('--pdf', help="Export a pdf file (requires chromium installed).", action="store_true")
+    parser.add_argument(
+            '--include',
+            metavar="RESOURCE",
+            help="Directory or file to include. "
+            "This option can be used multiple times.",
+            default=[],
+            action="append"
+        )
+    parser.add_argument(
+            '--pdf',
+            help="Export a pdf file (requires chromium installed).",
+            action="store_true"
+        )
     args = parser.parse_args()
     export_to_pdf = args.pdf
     export_to_html = not export_to_pdf
@@ -59,72 +72,73 @@ def main():
     title_re = r"#[#]*[ ]*(.*)"
     title_re = re.compile(title_re)
 
-    slide_delimitator = "!!!"
-    comment_char = "%"
-
     default_attributes = ""
     default_theme = "white"
     default_code_theme = "zenburn"
     default_options = {
-            "controls" : "false",
-            "markdown" : "{smartypants: true}",
+            "controls": "false",
+            "markdown": "{smartypants: true}",
         }
 
     # Open markdown file
-    with open(markdown_file) as f: presentation_markdown = list(f)
-
+    with open(markdown_file) as f:
+        presentation_markdown = list(f)
 
     # Build presentation
     presentation = list()
     slide = list()
-    options = ["{} : {},".format(key, val) for key, val in default_options.items()]
+    options = ["{} : {},".format(key, val)
+               for key, val in default_options.items()]
     theme = default_theme
     code_theme = default_code_theme
     attributes = default_attributes
     title = None
-    for l in presentation_markdown:
-        #l = l.strip()
-        l = l[:-1]
+    for line in presentation_markdown:
+        line = line[:-1]
 
         # Is the line setting an option?
-        m = option_re.match(l)
+        m = option_re.match(line)
         if m is not None:
             options.append("{} : {},".format(m.group(1), m.group(2)))
             continue
 
         # Is the line a slide break?
-        m = delimiter_re.match(l)
+        m = delimiter_re.match(line)
         if m is not None:
             attributes = default_attributes + " " + m.group(1)
-            presentation.append(section_template.format(attributes, "\n".join(slide)))
+            presentation.append(
+                    section_template.format(attributes, "\n".join(slide))
+                )
             slide = list()
             continue
 
         # Is the line setting a theme?
-        m = theme_re.match(l)
+        m = theme_re.match(line)
         if m is not None:
             theme = m.group(1)
             continue
 
         # Is the line setting a code theme?
-        m = code_theme_re.match(l)
+        m = code_theme_re.match(line)
         if m is not None:
             code_theme = m.group(1)
             continue
 
         # Is the line the first heading?
-        m = title_re.match(l)
+        m = title_re.match(line)
         if m is not None:
             h1 = m.group(1)
             if title is None:
                 title = h1
 
         # Else, we assume the line is markdown
-        slide.append(l)
+        slide.append(line)
 
     # Did the user forget to insert the final slide break?
     if len(slide) > 0:
-        presentation.append(section_template.format(default_attributes, "\n".join(slide)))
+        presentation.append(
+                section_template.format(default_attributes, "\n".join(slide))
+            )
 
     # Replacement strings
     if title is None:
@@ -144,15 +158,20 @@ def main():
         index_html = list(f)
 
     # Replace title
-    index_html = [l if title_word not in l else title for l in index_html]
+    index_html = [line if title_word not in line else title
+                  for line in index_html]
     # Replace theme
-    index_html = [l if theme_word not in l else theme for l in index_html]
+    index_html = [line if theme_word not in line else theme
+                  for line in index_html]
     # Replace code theme
-    index_html = [l if code_theme_word not in l else code_theme for l in index_html]
+    index_html = [line if code_theme_word not in line else code_theme
+                  for line in index_html]
     # Replace presentation
-    index_html = [l if magic_word not in l else presentation for l in index_html]
+    index_html = [line if magic_word not in line else presentation
+                  for line in index_html]
     # Replace options
-    index_html = [l if options_word not in l else options for l in index_html]
+    index_html = [line if options_word not in line else options
+                  for line in index_html]
 
     with open(index_file_new, "w") as f:
         f.write("".join(index_html))
@@ -160,14 +179,18 @@ def main():
     # Copy include files
     for path in [Path(p) for p in args.include]:
         if path.is_dir:
-            shutil.copytree(path, revealjs_dir/path.parts[-1], dirs_exist_ok=True)
+            shutil.copytree(
+                    path, revealjs_dir/path.parts[-1], dirs_exist_ok=True
+                )
         else:
             shutil.copy(path, revealjs_dir/path.parts[-1], exist_ok=True)
 
     # Export to PDF if needed
     if export_to_pdf:
         try:
-            pdf_chromium_export(index_file_new, markdown_file.with_suffix('.pdf'))
+            pdf_chromium_export(
+                    index_file_new, markdown_file.with_suffix('.pdf')
+                )
             print("Wrote {}".format(markdown_file.with_suffix('.pdf')))
 
         except Exception as e:
@@ -177,7 +200,9 @@ def main():
 
     if export_to_html:
         # Change output folder name
-        print("Done. Open {} with your web browser".format(revealjs_dir/"index.html"))
+        print("Done. Open {} with your web browser".format(
+                revealjs_dir/"index.html")
+              )
 
 
 if __name__ == "__main__":
