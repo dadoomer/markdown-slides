0395919816d69284294b936d63e476fceeb9e0af
Leo <leohdz.c0@gmail.com>
2021-01-10 14:23:45 -0600

    Version bump 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ revealjsdata = glob.glob("mdslides/reveal.js/**", recursive=True)
 revealjsdata = [str(Path(p).relative_to("mdslides")) for p in revealjsdata]
 
 setup(name='MarkdownSlides',
-      version='1.0',
+      version='1.1',
       description='Write modern slides with markdown.',
       packages=['mdslides'],
       entry_points={'console_scripts': ['mdslides=mdslides.__main__:main']},
